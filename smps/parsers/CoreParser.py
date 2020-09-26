29f554a506106afb842aa2a3074d9455caa029fe
Niels Wouda <nielswouda@gmail.com>
2020-09-26 12:24:46 +0200

    Stubs for various parts of the core file

--- smps/parsers/CoreParser.py
+++ smps/parsers/CoreParser.py
@@ -11,11 +11,11 @@ class CoreParser(Parser):
     FILE_EXTENSIONS = [".cor", ".COR", ".core", ".CORE"]
     STEPS = {
         "NAME": lambda self, data_line: self._process_name(data_line),
-        "ROWS": lambda self, data_line: None,  # TODO
-        "COLUMNS": lambda self, data_line: None,  # TODO
-        "RHS": lambda self, data_line: None,  # TODO
-        "BOUNDS": lambda self, data_line: None,  # TODO
-        "RANGES": lambda self, data_line: None,  # TODO
+        "ROWS": lambda self, data_line: self._process_rows(data_line),
+        "COLUMNS": lambda self, data_line: self._process_columns(data_line),
+        "RHS": lambda self, data_line: self._process_rhs(data_line),
+        "BOUNDS": lambda self, data_line: self._process_bounds(data_line),
+        "RANGES": lambda self, data_line: self._process_ranges(data_line),
     }
 
     def _process_name(self, data_line: DataLine):
@@ -24,3 +24,18 @@ class CoreParser(Parser):
         else:
             warnings.warn("Core file has no value for the NAME field.")
             logger.warning("Core file has no value for the NAME field.")
+
+    def _process_row(self, data_line: DataLine):
+        pass  # TODO
+
+    def _process_columns(self, data_line: DataLine):
+        pass  # TODO
+
+    def _process_rhs(self, data_line: DataLine):
+        pass  # TODO
+
+    def _process_bounds(self, data_line: DataLine):
+        pass  # TODO
+
+    def _process_ranges(self, data_line: DataLine):
+        pass  # TODO
