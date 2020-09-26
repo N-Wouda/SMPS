8eaa80536ad29df304484104bb036a1eae6d1e7d
Niels Wouda <nielswouda@gmail.com>
2020-09-26 17:49:16 +0200

    Parse RHS and BOUNDS

--- smps/parsers/CoreParser.py
+++ smps/parsers/CoreParser.py
@@ -45,10 +45,13 @@ class CoreParser(Parser):
         # of (constraint, variable, value)-tuples.
         self._elements: List[Tuple[str, str, float]] = []
         self._variable_names = []
-        self._types: np.array = []
+        self._types = []
         self._lb: np.array = []
         self._ub: np.array = []
         self._var2idx = {}
+
+        # Flag for parsing integers
+        self._parse_ints = False
 
     @property
     def constraint_names(self) -> List[str]:
@@ -128,6 +131,16 @@ class CoreParser(Parser):
         return self._variable_names
 
     @property
+    def types(self) -> np.array:
+        """
+        Returns the variable types, as an array. These types are 'I' for
+        integer, 'C' for a continuous variable, and 'B' for a binary variable.
+        The first type belongs to the first variable, the second to the second
+        variable, and so on.
+        """
+        return np.array(self._types)
+
+    @property
     def lower_bounds(self) -> np.array:
         """
         Variable lower bounds, as a vector with one entry per variable. If this
@@ -174,42 +187,107 @@ class CoreParser(Parser):
             self._constr2idx[data_line.name()] = len(self._constraint_names) - 1
 
     def _process_columns(self, data_line: DataLine):
-        var = data_line.name()
+        name = data_line.first_data_name()
 
-        if var not in self._var2idx:
-            self._variable_names.append(var)
-            self._var2idx[var] = len(self._variable_names) - 1
-
-        constr = data_line.first_data_name()
-        value = data_line.first_number()
-        self._add(constr, var, value)
-
-        if data_line.has_second_data_entry():
-            constr = data_line.second_data_name()
-            value = data_line.second_number()
-            self._add(constr, var, value)
+        if "MARKER" in name.upper():
+            self._parse_marker(data_line)
+        else:
+            self._parse_column(data_line)
 
     def _process_rhs(self, data_line: DataLine):
         if len(self._rhs) != len(self.constraint_names):
             self._rhs = np.zeros(len(self.constraint_names))
 
-        # TODO
+        self._add_rhs(data_line.first_data_name(), data_line.first_number())
+
+        if data_line.has_second_data_entry():
+            self._add_rhs(data_line.second_data_name(),
+                          data_line.second_number())
 
     def _process_bounds(self, data_line: DataLine):
+        """
+        There are a ton of bound types, but the most common are listed below,
+        originally due to http://lpsolve.sourceforge.net/5.5/mps-format.htm. A
+        bound is specified by a two-letter type and a value b.
+
+        - LO    lower bound        b <= x (< +inf)
+        - UP    upper bound        (0 <=) x <= b
+        - FX    fixed variable     x = b
+        - FR    free variable      -inf < x < +inf
+        - MI    lower bound -inf   -inf < x (<= 0)
+        - PL    upper bound +inf   (0 <=) x < +inf
+        - BV    binary variable    x = 0 or 1
+        - LI    integer variable   b <= x (< +inf)
+        - UI    integer variable   (0 <=) x <= b
+
+        Raises
+        ------
+        ValueError
+            When the bound type is not understood.
+        """
         if len(self._lb) != len(self._ub) != len(self.variable_names):
             self._lb = np.zeros(len(self.variable_names))
             self._ub = np.full(len(self.variable_names), np.inf)
 
-        # TODO
+        types = {"FR", "MI", "PL", "BV", "LO", "UP", "FX", "LI", "UI"}
+        bound_type = data_line.indicator()
+
+        if bound_type not in types:
+            msg = f"Bounds of type {bound_type} are not understood."
+            logger.error(msg)
+            raise ValueError(msg)
+
+        var = data_line.first_data_name()
+        idx = self._var2idx[var]
+
+        # The value is clear from these types, and need not have been specified.
+        # Hence we treat these separately, and then return.
+        if bound_type in {"FR", "MI", "PL", "BV"}:
+            if bound_type == "FR":  # free variable
+                self._lb[idx] = -np.inf
+                self._ub[idx] = np.inf
+
+            if bound_type == "MI":  # -inf lower bound
+                self._lb[idx] = -np.inf
+
+            if bound_type == "PL":  # +inf upper bound
+                self._ub[idx] = np.inf
+
+            if bound_type == "BV":  # binary variable
+                self._lb[idx] = 0
+                self._ub[idx] = 1
+                self._types[idx] = 'B'
+
+            return
+
+        value = data_line.first_number()
+
+        if bound_type == "LO":  # lower bound
+            self._lb[idx] = value
+
+        if bound_type == "UP":  # upper bound
+            self._ub[idx] = value
+
+        if bound_type == "FX":  # fixed variable
+            self._lb[idx] = value
+            self._ub[idx] = value
+
+        if bound_type == "LI":  # integer variable, lower bound
+            self._lb[idx] = value
+            self._types[idx] = 'I'
+
+        if bound_type == "UI":  # integer variable, upper bound
+            self._ub[idx] = value
+            self._types[idx] = 'I'
 
     def _process_ranges(self, data_line: DataLine):
         if len(self._lb) != len(self._ub) != len(self.variable_names):
             self._lb = np.zeros(len(self.variable_names))
             self._ub = np.full(len(self.variable_names), np.inf)
 
-        # TODO
+        # TODO see https://github.com/N-Wouda/SMPS/issues/5
 
-    def _add(self, constr: str, var: str, value: float):
+    def _add_value(self, constr: str, var: str, value: float):
         if constr == self.objective_name:
             self._obj_coeffs.append((var, value))
 
@@ -218,4 +296,47 @@ class CoreParser(Parser):
 
         if constr != self.objective_name and constr not in self._constr2idx:
             # This is likely a "no restriction" row other than the objective.
-            logger.info(f"Constraint {constr} is not understood, and skipped.")
+            logger.info(f"Constraint {constr} is not understood; skipping.")
+
+    def _add_rhs(self, constr: str, value: float):
+        if constr in self._constr2idx:
+            idx = self._constr2idx[constr]
+            self._rhs[idx] = value
+        else:
+            # A right-hand side must be associated with an actual constraint, so
+            # this cannot have been a "no restriction" row. It is likely an
+            # issue in the core file.
+            msg = f"Cannot add RHS for unknown constraint {constr}; skipping."
+            logger.warning(msg)
+            warnings.warn(msg)
+
+    def _parse_marker(self, data_line: DataLine):
+        assert data_line.has_second_data_entry()
+
+        name = data_line.name()
+        marker_type = data_line.second_data_name()
+
+        logger.debug(f"Encountered a {marker_type} marker named {name}.")
+
+        if "INTORG" in marker_type.upper():
+            self._parse_ints = True
+
+        if "INTEND" in marker_type.upper():
+            self._parse_ints = False
+
+    def _parse_column(self, data_line: DataLine):
+        var = data_line.name()
+
+        if var not in self._var2idx:
+            self._variable_names.append(var)
+            self._types.append('I' if self._parse_ints else 'C')
+            self._var2idx[var] = len(self._variable_names) - 1
+
+        constr = data_line.first_data_name()
+        value = data_line.first_number()
+        self._add_value(constr, var, value)
+
+        if data_line.has_second_data_entry():
+            constr = data_line.second_data_name()
+            value = data_line.second_number()
+            self._add_value(constr, var, value)
