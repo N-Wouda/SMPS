d3b687d900b8340d8b9e35cd49bd99c226a520d6
Niels Wouda <nielswouda@gmail.com>
2020-10-18 14:10:31 +0200

    Stub for Indep tests

--- smps/classes/tests/test_Indep.py
+++ smps/classes/tests/test_Indep.py
@@ -1,1 +1,34 @@
-# TODO
+def test_raises_distribution():
+    pass
+
+
+def test_raises_modification():
+    pass
+
+
+def test_add_distribution():
+    pass
+
+
+def test_uniform():
+    pass
+
+
+def test_normal():
+    pass
+
+
+def test_gamma():
+    pass
+
+
+def test_beta():
+    pass
+
+
+def test_lognorm():
+    pass
+
+
+def test_discrete():
+    pass
