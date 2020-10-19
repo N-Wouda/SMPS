3ba3d0df29c8e3035a6ace1320c9cdec45a12898
Niels Wouda <nielswouda@gmail.com>
2020-10-19 22:14:03 +0200

    Some tests, example file

--- smps/__init__.py
+++ smps/__init__.py
@@ -1,1 +1,2 @@
+from .read_mps import read_mps
 from .read_smps import read_smps
--- smps/tests/test_read_mps.py
+++ smps/tests/test_read_mps.py
@@ -1,1 +1,26 @@
-# TODO
+from pathlib import Path
+
+from numpy.testing import assert_equal, assert_raises
+
+from smps import read_mps
+
+
+def test_raises_file_does_not_exist():
+    with assert_raises(FileNotFoundError):
+        # Weird string that should not exist.
+        read_mps("bogus_location/as2afsd76a")
+
+    read_mps("data/test/mps_test_file_small")  # does exist, so should work.
+
+
+def test_file_location():
+    res = read_mps("data/test/mps_test_file_small")
+    assert_equal(res.mps_location, Path("data/test/mps_test_file_small.mps"))
+
+
+def test_small_example():
+    """
+    Tests if a small example MPS file is parsed correctly.
+    """
+    res = read_mps("data/test/mps_test_file_small")
+    # TODO
